ab123e08ee51e2a497659d22fd1c0cc63ae29a28
Elvisthacoder <ekk213@gmail.com>
2016-04-14 15:57:48 +0300

    listener

--- app/src/main/java/listener/OnValueChangeListener.java
+++ app/src/main/java/listener/OnValueChangeListener.java
@@ -4,4 +4,13 @@ package listener;
  * Created by taifa on 4/14/16.
  */
 public interface OnValueChangeListener {
+
+
+    /**
+     * Invoked when the value changes on the {@link ActualNumberPicker}. Remember to set this listener to the View.
+     *
+     * @param oldValue The value picker had before the change
+     * @param newValue Tha value picker has now, after the change
+     */
+    void onValueChanged(int oldValue, int newValue);
 }
