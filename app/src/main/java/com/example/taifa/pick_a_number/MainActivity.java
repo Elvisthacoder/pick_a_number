190c3d0ce61e84953e4a66b683f21a7f7ecca6f7
Elvisthacoder <ekk213@gmail.com>
2016-04-14 10:18:12 +0300

    implements_main

--- app/src/main/java/com/example/taifa/pick_a_number/MainActivity.java
+++ app/src/main/java/com/example/taifa/pick_a_number/MainActivity.java
@@ -2,8 +2,9 @@ package com.example.taifa.pick_a_number;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.NumberPicker;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnValueChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
