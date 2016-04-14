8addd96bfcdfdf361db86916cb8de71686482bec
Elvisthacoder <ekk213@gmail.com>
2016-04-14 18:25:45 +0300

    static files

--- app/src/main/java/view/Coloring.java
+++ app/src/main/java/view/Coloring.java
@@ -4,4 +4,13 @@ package view;
  * Created by taifa on 4/14/16.
  */
 public class Coloring {
+
+    private static final String LOG_TAG = Coloring.class.getSimpleName();
+    private static final int BOUNDS = 1500;
+    private static final int BRIGHTNESS_THRESHOLD = 180;
+    private static final int FADE_DURATION = 200;
+
+    private static final Object mInitializerLock;
+    private static Coloring mInstance;
+
 }
